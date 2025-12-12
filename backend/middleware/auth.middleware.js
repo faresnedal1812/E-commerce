591b37701acb7610e640c00f83f2e4a066634b1b
faresnedal1812 <nedalfares53@gmail.com>
2025-12-12 17:49:17 +0200

    add protect route and admin route middlewares, and create product model

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,44 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    const access_token = req.cookie.access_token;
+    if (!access_token) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized - No access token provided" });
+    }
+
+    try {
+      const decoded = jwt.verify(access_token, process.env.ACCESS_TOKEN_SECRET);
+      const user = await User.findById(decoded.userId);
+
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      req.user = user;
+      console.log(req.user);
+      next();
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        return res
+          .status(401)
+          .json({ message: "Unauthorized - Access token expired" });
+      }
+      throw error;
+    }
+  } catch (error) {
+    console.log("Error in protectRoute middleware:", error.messaeg);
+    return res.status(500).json({ message: "Invalid access token" });
+  }
+};
+
+export const adminRoute = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    return res.status(403).json({ message: "Access denied - Admin only" });
+  }
+};

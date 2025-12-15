629d5cd3804823901caa2f210b0a3750c4501d38
faresnedal1812 <nedalfares53@gmail.com>
2025-12-15 18:13:44 +0200

    refactor(cart): clean up cart controllers and routes, feat(product): implement full product functoinality

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -1,93 +1,4 @@
-// import Product from "./../models/product.model.js";
 import Product from "./../models/product.model.js";
-
-// // TODO: after the testing of all routes => i think this line const existingItem = user.cartItems.find((item) => item.id === productId); must be const existingItem = user.cartItems.find((item) => item.product.toString === productId);
-
-// export const getCartProducts = async (req, res) => {
-//   try {
-//     const products = await Product.find({ _id: { $in: req.user.cartItems } });
-
-//     // add quantity for each product
-//     // item.id is alias to item._id.toString() in Mongoose
-//     // find() => return the first value that apply the conditions
-//     const cartItems = products.map((product) => {
-//       const item = req.user.cartItems.find((item) => item.id === product.id);
-//       return { ...product.toJSON(), quantity: item.quantity };
-//     });
-
-//     res.status(200).json(cartItems);
-//   } catch (error) {
-//     console.log("Error in getCartProduct controller:", error.message);
-//     res.status(500).json({ message: "Internal server error" });
-//   }
-// };
-
-// export const addToCart = async (req, res) => {
-//   try {
-//     const { productId } = req.body;
-//     const user = req.user;
-
-//     const existingItem = user.cartItems.find((item) => item.id === productId);
-
-//     if (existingItem) {
-//       existingItem.quantity += 1;
-//     } else {
-//       user.existingItem.push(productId);
-//     }
-
-//     await user.save();
-//     res.status(200).json(user.cartItems);
-//   } catch (error) {
-//     console.log("Error in addToCart controller:", error.message);
-//     res.status(500).json({ message: "Internal server error" });
-//   }
-// };
-
-// export const removeAllFromCart = async (req, res) => {
-//   try {
-//     const { productId } = req.body;
-//     const user = req.user;
-
-//     if (!productId) {
-//       user.cartItems = [];
-//     } else {
-//       user.cartItems = user.cartItems.filter((item) => item.id !== productId);
-//     }
-
-//     await user.save();
-//     res.status(200).json(user.cartItems);
-//   } catch (error) {
-//     console.log("Error in removeAllFromCart controller:", error.message);
-//     res.status(500).json({ message: "Internal server error" });
-//   }
-// };
-
-// export const updateQuantity = async (req, res) => {
-//   try {
-//     const { id: productId } = req.params;
-//     const { quantity } = req.body;
-
-//     const user = req.user;
-//     const existingItem = user.cartItems.find((item) => item.id === productId);
-
-//     if (existingItem) {
-//       if (quantity === 0) {
-//         user.cartItems = user.cartItems.filter((item) => item.id !== productId);
-//         await user.save();
-//         return res.status(200).json(user.cartItems);
-//       }
-
-//       existingItem.quantity = quantity;
-//       await user.save();
-//       res.status(200).json(user.cartItems);
-//     } else {
-//       res.status(404).json({ message: "Product not found" });
-//     }
-//   } catch (error) {
-//     console.log("Error in updateQuantity controller:", error.message);
-//     res.status(500).json({ message: "Internal server error" });
-//   }
-// };
 
 export const getCartProducts = async (req, res) => {
   // item.id is alias to item._id.toString() in Mongoose
@@ -151,14 +62,14 @@ export const removeAllFromCart = async (req, res) => {
       user.cartItems = [];
     } else {
       user.cartItems = user.cartItems.filter(
-        (item) => item.product.toString() != productId
+        (item) => item.product.toString() !== productId
       );
     }
 
     await user.save();
     res.status(200).json(user.cartItems);
   } catch (error) {
-    console.log("Error in addToCart controller:", error.message);
+    console.log("Error in removeAllFromCart controller:", error.message);
     res.status(500).json({ message: "Internal server error" });
   }
 };
@@ -167,6 +78,12 @@ export const updateQuantity = async (req, res) => {
   try {
     const { id: productId } = req.params;
     const { quantity } = req.body;
+
+    if (typeof quantity !== "number" || quantity < 0) {
+      return res
+        .status(400)
+        .json({ message: "quantity must be a non-negative number" });
+    }
 
     const user = req.user;
 
@@ -187,7 +104,7 @@ export const updateQuantity = async (req, res) => {
       await user.save();
       res.status(200).json(user.cartItems);
     } else {
-      res.status(404).json({ messgae: "Product not found in cart" });
+      res.status(404).json({ message: "Product not found in cart" });
     }
   } catch (error) {
     console.log("Error in updateQuantity controller:", error.message);

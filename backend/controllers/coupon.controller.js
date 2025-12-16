532bf0ee528bae90bd7723d4e3a6b27d0827fa1c
faresnedal1812 <nedalfares53@gmail.com>
2025-12-16 16:01:54 +0200

    coupon routes setup

---
+++ backend/controllers/coupon.controller.js
@@ -0,0 +1,45 @@
+import Coupon from "../models/coupon.model.js";
+
+export const getCoupon = async (req, res) => {
+  try {
+    const coupon = await Coupon.findOne({
+      userId: req.user._id,
+      isActive: true,
+    });
+
+    res.status(200).json(coupon || null);
+  } catch (error) {
+    console.log("Error in getCoupon controller:", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const validateCoupon = async (req, res) => {
+  try {
+    const { code } = req.body;
+    const coupon = await Coupon.findOne({
+      code,
+      userId: req.user._id,
+      isActive: true,
+    });
+
+    if (!coupon) {
+      return res.status(404).json({ message: "Coupon not found" });
+    }
+
+    if (coupon.expirationDate < Date.now()) {
+      coupon.isActive = false;
+      await coupon.save();
+      return res.status(400).json({ message: "Coupon expired" });
+    }
+
+    res.status(200).json({
+      message: "Coupon is valid",
+      code: coupon.code,
+      discountPercentage: coupon.discountPercentage,
+    });
+  } catch (error) {
+    console.log("Error in validateCoupon controller:", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};

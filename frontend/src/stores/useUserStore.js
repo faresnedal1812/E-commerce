fb813d51c6e781c07199c18fc53bdd37702fb63e
faresnedal1812 <nedalfares53@gmail.com>
2025-12-23 15:44:19 +0200

    feat(auth): complete authentication functionalities

---
+++ frontend/src/stores/useUserStore.js
@@ -0,0 +1,63 @@
+import { create } from "zustand";
+import { toast } from "react-hot-toast";
+import axios from "../lib/axios";
+
+export const useUserStore = create((set, get) => ({
+  user: null,
+  loading: false,
+  checkingAuth: true,
+
+  signup: async ({ name, email, password, confirmPassword }) => {
+    set({ loading: true });
+
+    if (password !== confirmPassword) {
+      set({ loading: false });
+      return toast.error("Password do not match");
+    }
+
+    try {
+      const res = await axios.post("/auth/signup", { name, email, password });
+      set({ user: res.data, loading: false });
+      toast.success("User signed up successfully");
+    } catch (error) {
+      set({ loading: false });
+      toast.error(error.response?.data?.message || "An error occurred");
+    }
+  },
+
+  login: async (email, password) => {
+    set({ loading: true });
+
+    try {
+      const res = await axios.post("/auth/login", { email, password });
+      set({ user: res.data, loading: false });
+    } catch (error) {
+      set({ loading: false });
+      toast.error(error.response?.data?.message || "An error occurred");
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axios.post("/auth/logout");
+      set({ user: null });
+    } catch (error) {
+      toast.error(
+        error.response?.data?.message || "An error aoccurred during logout"
+      );
+    }
+  },
+
+  checkAuth: async () => {
+    set({ checkingAuth: true });
+    try {
+      const res = await axios.get("/auth/getProfile");
+      set({ user: res.data, checkingAuth: false });
+    } catch (error) {
+      set({ user: null, checkingAuth: false });
+      console.log(error.message);
+    }
+  },
+}));
+
+// TODO: create refresh token to create new access token after 15m
